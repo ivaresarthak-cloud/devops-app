80a005bb199e5072b4c79bb85d3591ba79de87e6
ivaresarthak-cloud <ivaresarthak@gmail.com>
2026-03-31 09:05:38 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('DevOps Project Running!');
+});
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
